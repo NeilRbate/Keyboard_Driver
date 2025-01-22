590c02a0503c025d7c9533ec31bbc0e354297bf4
Julien Barbate <julienb@rbate.fr>
2025-01-22 18:02:53 +0100

    [Add]Add keylist and print of key on kernel consol

--- src/main.c
+++ src/main.c
@@ -1,247 +1,161 @@
 #include "../include/include.h"
 
-static void keyboard_disconnect(struct usb_interface *interface);
-static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id);
-
-
-struct usb_keyboard {
-    struct usb_device *udev;
-    struct input_dev *input;
-    struct urb *irq_urb;  // URB for receiving HID reports
-    unsigned char *irq_buf;  // Buffer for HID reports
-};
-
-// This function will parse the HID report and send key events
 static int parse_hid_report(struct usb_keyboard *keyboard, unsigned char *data, int size)
 {
-    struct input_dev *input = keyboard->input;
-    unsigned char *keycode;
-    int i;
+	struct input_dev *input;
+	unsigned char *keycode;
+	int i;
 
-    // Assuming the report format for a USB keyboard (HID boot protocol)
-    // The first byte is the modifier byte (e.g., Shift, Ctrl)
-    // The following bytes (from index 2 to 7) represent key codes (typically 6 bytes)
+	if (size < 8) {
+		pr_err("invalid report size\n");
+		return -EINVAL;
+	}
 
-    if (size < 8) {
-        printk(KERN_WARNING "Invalid report size\n");
-        return -EINVAL;
-    }
+	input = keyboard->input;
 
-    keycode = &data[2]; // Starting from the 3rd byte (key codes)
+	//Get the 3rd byte, the [0] and [1] are modifier byte
+	keycode = &data[2];
 
-    // Report key presses
-    for (i = 0; i < 6; i++) {
-        if (keycode[i]) {
-            input_report_key(input, keycode[i], 1);  // Key down
-        }
-    }
+	//Key down
+	for (i = 0; i < 6; i++) {
+		if (keycode[i] && keycode[i] < sizeof(keycode_to_name)/sizeof(keycode_to_name[0])) {
+			printk(KERN_INFO "Key Pressed: %s\n", keycode_to_name[keycode[i]]);
+			input_report_key(input, keycode[i], KEY_DOWN);
+		}
+	}
+	input_sync(input);
 
-    input_sync(input);
+	//Key up
+	for (i = 0; i < 6; i++) {
+		if (keycode[i]) {
+			input_report_key(input, keycode[i], KEY_UP);
+		}
+	}
+	input_sync(input);
 
-    // Report key releases
-    for (i = 0; i < 6; i++) {
-        if (keycode[i]) {
-            input_report_key(input, keycode[i], 0);  // Key up
-        }
-    }
-
-    input_sync(input);
-    return 0;
+	return 0;
 }
 
 static void keyboard_irq(struct urb *urb)
 {
-    struct usb_keyboard *keyboard = urb->context;
-    unsigned char *data = urb->transfer_buffer;
-    int size = urb->actual_length;
+	struct usb_keyboard *keyboard;
+	unsigned char *data;
+	int size;
 
-    if (urb->status) {
-        printk(KERN_ERR "URB error: %d\n", urb->status);
-        return;
-    }
+	keyboard = urb->context;
+	data = urb->transfer_buffer;
+       	size = urb->actual_length;
 
-    // Parse the HID report
-    parse_hid_report(keyboard, data, size);
+	if (urb->status) {
+		pr_err("URB error: %d\n", urb->status);
+		return;
+	}
 
-    // Resubmit the URB to receive the next report
-    usb_submit_urb(urb, GFP_ATOMIC);
+	parse_hid_report(keyboard, data, size);
+	usb_submit_urb(urb, GFP_ATOMIC);
 }
 
 static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-    struct usb_keyboard *keyboard;
-    struct input_dev *input_dev;
-    int retval;
+	struct usb_keyboard *keyboard;
+	struct input_dev *input_dev;
+	int res;
 
-    // Allocate memory for keyboard structure
-    keyboard = kzalloc(sizeof(struct usb_keyboard), GFP_KERNEL);
-    if (!keyboard)
-        return -ENOMEM;
+	keyboard = kzalloc(sizeof(struct usb_keyboard), GFP_KERNEL);
+	if (!keyboard) {
+		pr_err("failed to allocated memory\n");
+		return -ENOMEM;
+	}
+	keyboard->udev = usb_get_dev(interface_to_usbdev(interface));
 
-    keyboard->udev = usb_get_dev(interface_to_usbdev(interface));
+	input_dev = input_allocate_device();
+	if (!input_dev) {
+		pr_err("failed to allocated memory\n");
+		kfree(keyboard);
+		return -ENOMEM;
+	}
 
-    // Create input device
-    input_dev = input_allocate_device();
-    if (!input_dev) {
-        kfree(keyboard);
-        return -ENOMEM;
-    }
+	input_dev->name = "Custom USB Keyboard";
+	input_dev->phys = "usb/keyboard";
+	input_dev->id.bustype = BUS_USB;
+	input_dev->evbit[0] = BIT(EV_KEY) | BIT(EV_SYN);
+	input_dev->keybit[BIT_WORD(KEY_A)] = BIT(KEY_A) | BIT(KEY_B);  // Example keys
 
-    input_dev->name = "Custom USB Keyboard";
-    input_dev->phys = "usb/keyboard";
-    input_dev->id.bustype = BUS_USB;
-    input_dev->evbit[0] = BIT(EV_KEY) | BIT(EV_SYN);
-    input_dev->keybit[BIT_WORD(KEY_A)] = BIT(KEY_A) | BIT(KEY_B);  // Example keys
+	res = input_register_device(input_dev);
+	if (res) {
+		pr_err("input_register_device failed. error number %d\n", res);
+		input_free_device(input_dev);
+		kfree(keyboard);
+		return res;
+	}
 
-    // Register the input device
-    retval = input_register_device(input_dev);
-    if (retval) {
-        input_free_device(input_dev);
-        kfree(keyboard);
-        return retval;
-    }
+	keyboard->input = input_dev;
 
-    keyboard->input = input_dev;
+	keyboard->irq_buf = kmalloc(8, GFP_KERNEL);
+	if (!keyboard->irq_buf) {
+		pr_err("failed to allocated memory\n");
+		input_unregister_device(input_dev);
+		kfree(keyboard);
+		return -ENOMEM;
+	}
 
-    // Allocate memory for the URB and buffer
-    keyboard->irq_buf = kmalloc(8, GFP_KERNEL);  // Buffer for a single HID report
-    if (!keyboard->irq_buf) {
-        input_unregister_device(input_dev);
-        kfree(keyboard);
-        return -ENOMEM;
-    }
+	keyboard->irq_urb = usb_alloc_urb(0, GFP_KERNEL);
+	if (!keyboard->irq_urb) {
+		pr_err("usb_alloc_urb failed\n");
+		kfree(keyboard->irq_buf);
+		input_unregister_device(input_dev);
+		kfree(keyboard);
+		return -ENOMEM;
+	}
 
-    keyboard->irq_urb = usb_alloc_urb(0, GFP_KERNEL);
-    if (!keyboard->irq_urb) {
-        kfree(keyboard->irq_buf);
-        input_unregister_device(input_dev);
-        kfree(keyboard);
-        return -ENOMEM;
-    }
+	usb_fill_int_urb(keyboard->irq_urb, keyboard->udev,
+			usb_rcvintpipe(keyboard->udev, 0x81),  // Endpoint 0x81 for IN (Interrupt)
+			keyboard->irq_buf, 8,                  // 8 bytes for the HID report
+			keyboard_irq, keyboard, 10);          // 10ms polling interval
 
-    // Set up the URB to receive reports from the keyboard
-    usb_fill_int_urb(keyboard->irq_urb, keyboard->udev,
-                     usb_rcvintpipe(keyboard->udev, 0x81),  // Endpoint 0x81 for IN (Interrupt)
-                     keyboard->irq_buf, 8,                  // 8 bytes for the HID report
-                     keyboard_irq, keyboard, 10);          // 10ms polling interval
+	res = usb_submit_urb(keyboard->irq_urb, GFP_KERNEL);
+	if (res) {
+		pr_err("usb_submit_urb failed\n");
+		usb_free_urb(keyboard->irq_urb);
+		kfree(keyboard->irq_buf);
+		input_unregister_device(input_dev);
+		kfree(keyboard);
+		return res;
+	}
 
-    // Submit the URB to start receiving HID reports
-    retval = usb_submit_urb(keyboard->irq_urb, GFP_KERNEL);
-    if (retval) {
-        usb_free_urb(keyboard->irq_urb);
-        kfree(keyboard->irq_buf);
-        input_unregister_device(input_dev);
-        kfree(keyboard);
-        return retval;
-    }
-
-    usb_set_intfdata(interface, keyboard);
-
-    return 0;
+	usb_set_intfdata(interface, keyboard);
+	return 0;
 }
 
 static void keyboard_disconnect(struct usb_interface *interface)
 {
-    struct usb_keyboard *keyboard = usb_get_intfdata(interface);
+	struct usb_keyboard *keyboard = usb_get_intfdata(interface);
 
-    // Cancel the URB and free the resources
-    usb_kill_urb(keyboard->irq_urb);
-    usb_free_urb(keyboard->irq_urb);
-    kfree(keyboard->irq_buf);
+	usb_kill_urb(keyboard->irq_urb);
+	usb_free_urb(keyboard->irq_urb);
+	kfree(keyboard->irq_buf);
 
-    input_unregister_device(keyboard->input);
-    usb_put_dev(keyboard->udev);
-    kfree(keyboard);
-}
-
-static struct usb_driver keyboard_driver = {
-    .name = "usb_keyboard_driver",
-    .id_table = keyboard_table,
-    .probe = keyboard_probe,
-    .disconnect = keyboard_disconnect,
-};
-
-/*
-struct usb_keyboard {
-    struct usb_device *udev;
-    struct input_dev *input;
-};
-
-static struct usb_driver keyboard_driver = {
-	.name = "my_keyboard_driver",
-	.probe = keyboard_probe,
-	.disconnect = keyboard_disconnect,
-	.id_table = keyboard_table,
-};
-
-MODULE_DEVICE_TABLE (usb, keyboard_table);
-
-static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id)
-{
-	struct input_dev 	*input_device;
-	struct usb_keyboard	*keyboard;
-	int 			result;
-
-	keyboard = kzalloc(sizeof(struct usb_keyboard), GFP_KERNEL);
-	if (!keyboard)
-		return -ENOMEM;
-
-	keyboard->udev = usb_get_dev(interface_to_usbdev(interface));
-
-	input_device = input_allocate_device();
-	if (!input_device) {
-		pr_err("input_allocate_device failedi\n");
-		kfree(keyboard);
-		return -ENOMEM;
-	}
-
-	input_device->name = "My keyboard";
-	input_device->phys = "usbkbd/input0";
-	input_device->id.bustype = BUS_USB;
-	input_device->evbit[0] = BIT(EV_KEY) | BIT(EV_SYN);
-	//Example, to change
-	input_device->keybit[BIT_WORD(KEY_A)] = BIT(KEY_A) | BIT(KEY_B);
-
-	result = input_register_device(input_device);
-	if (result) {
-		pr_err("input_register_device failed");
-		input_free_device(input_device);
-		kfree(keyboard);
-		return result;
-	}
-
-	keyboard->input = input_device;
-	usb_set_intfdata(interface, keyboard);
-	pr_debug("keyboard_probe is alive");
-	//Todo Check return value
-	
-	return 0;
-}
-
-static void keyboard_disconnect(struct usb_interface *interface) 
-{
-	struct usb_keyboard	*keyboard;
-
-	keyboard = usb_get_intfdata(interface);
 	input_unregister_device(keyboard->input);
 	usb_put_dev(keyboard->udev);
 	kfree(keyboard);
-
-	pr_debug("keyboard id disconnected\n");
 }
-*/
+
+static struct usb_driver keyboard_driver = {
+	.name = "my_keyboard_driver",
+	.id_table = keyboard_table,
+	.probe = keyboard_probe,
+	.disconnect = keyboard_disconnect,
+};
 
 static int __init keyboard_driver_init(void)
 {
-	pr_info("Init module\n");
+	int res;
 
-	int result;
-
-	result = usb_register(&keyboard_driver);
-	if (result) {
+	res = usb_register(&keyboard_driver);
+	if (res) {
 		pr_err("usb_register for %s failed. Error number %d\n",
-				keyboard_driver.name, result);
-		return result;
+				keyboard_driver.name, res);
+		return res;
 	}
 
 	return 0;
