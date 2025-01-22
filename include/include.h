1458ac23f6a7b80e3ba096ffac59d2f6313c236f
Julien Barbate <julienb@rbate.fr>
2025-01-22 15:24:05 +0100

    [ADD] Add usb support

--- include/include.h
+++ include/include.h
@@ -3,9 +3,11 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/printk.h>
 #include <linux/usb.h>
 #include <linux/input.h>
+#include <linux/hid.h>
 
 #define USB_KEYBOARD_VENDOR_ID 0x046d
 #define USB_KEYBOARD_PRODUCT_ID 0xc328
--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 static void keyboard_disconnect(struct usb_interface *interface);
 static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id);
 
+struct usb_keyboard {
+    struct usb_device *udev;
+    struct input_dev *input;
+};
+
 static struct usb_driver keyboard_driver = {
 	.name = "my_keyboard_driver",
 	.probe = keyboard_probe,
@@ -14,12 +19,20 @@ MODULE_DEVICE_TABLE (usb, keyboard_table);
 
 static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-	struct input_dev *input_device;
-	int result;
+	struct input_dev 	*input_device;
+	struct usb_keyboard	*keyboard;
+	int 			result;
+
+	keyboard = kzalloc(sizeof(struct usb_keyboard), GFP_KERNEL);
+	if (!keyboard)
+		return -ENOMEM;
+
+	keyboard->udev = usb_get_dev(interface_to_usbdev(interface));
 
 	input_device = input_allocate_device();
 	if (!input_device) {
 		pr_err("input_allocate_device failed");
+		kfree(keyboard);
 		return -ENOMEM;
 	}
 
@@ -27,16 +40,22 @@ static int keyboard_probe(struct usb_interface *interface, const struct usb_devi
 	input_device->phys = "usbkbd/input0";
 	input_device->id.bustype = BUS_USB;
 	input_device->evbit[0] = BIT(EV_KEY) | BIT(EV_SYN);
+	//Example, to change
+	input_device->keybit[BIT_WORD(KEY_A)] = BIT(KEY_A) | BIT(KEY_B);
 
-	set_bit(KEY_A, input_device->keybit);
-    	set_bit(KEY_B, input_device->keybit);
+	//set_bit(KEY_A, input_device->keybit);
+    	//set_bit(KEY_B, input_device->keybit);
 
 	result = input_register_device(input_device);
 	if (result) {
 		pr_err("input_register_device failed");
 		input_free_device(input_device);
+		kfree(keyboard);
 		return result;
 	}
+
+	keyboard->input = input_device;
+	usb_set_intfdata(interface, keyboard);
 	pr_debug("keyboard_probe is alive");
 	//Todo Check return value
 	
@@ -45,7 +64,11 @@ static int keyboard_probe(struct usb_interface *interface, const struct usb_devi
 
 static void keyboard_disconnect(struct usb_interface *interface) 
 {
-    printk(KERN_INFO "Keyboard disconnected\n");
+	struct usb_keyboard	*keyboard;
+
+	keyboard = usb_get_intfdata(interface);
+	input_unregister_device(keyboard->input);
+	kfree(keyboard);
 }
 
 static int __init keyboard_driver_init(void)
