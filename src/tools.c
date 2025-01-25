8487a47665c73def480de7df1f54447a9ffedf56
Julien Barbate <julienb@rbate.fr>
2025-01-25 12:03:22 +0100

    [ADD]Add linux keycode translate table

--- src/tools.c
+++ src/tools.c
@@ -168,6 +168,75 @@ const char *keycode_to_name[] = {
     "Page Down",               // 0xF1
 };
 
+const char hid_to_linux_keycode[256] = {
+    [0x00] = 0,
+    [0x04] = KEY_A,
+    [0x05] = KEY_B,
+    [0x06] = KEY_C,
+    [0x07] = KEY_D,
+    [0x08] = KEY_E,
+    [0x09] = KEY_F,
+    [0x0A] = KEY_G,
+    [0x0B] = KEY_H,
+    [0x0C] = KEY_I,
+    [0x0D] = KEY_J,
+    [0x0E] = KEY_K,
+    [0x0F] = KEY_L,
+    [0x10] = KEY_M,
+    [0x11] = KEY_N,
+    [0x12] = KEY_O,
+    [0x13] = KEY_P,
+    [0x14] = KEY_Q,
+    [0x15] = KEY_R,
+    [0x16] = KEY_S,
+    [0x17] = KEY_T,
+    [0x18] = KEY_U,
+    [0x19] = KEY_V,
+    [0x1A] = KEY_W,
+    [0x1B] = KEY_X,
+    [0x1C] = KEY_Y,
+    [0x1D] = KEY_Z,
+    [0x1E] = KEY_1,
+    [0x1F] = KEY_2,
+    [0x20] = KEY_3,
+    [0x21] = KEY_4,
+    [0x22] = KEY_5,
+    [0x23] = KEY_6,
+    [0x24] = KEY_7,
+    [0x25] = KEY_8,
+    [0x26] = KEY_9,
+    [0x27] = KEY_0,
+    [0x28] = KEY_ENTER,
+    [0x29] = KEY_ESC,
+    [0x2A] = KEY_BACKSPACE,
+    [0x2B] = KEY_TAB,
+    [0x2C] = KEY_SPACE,
+    [0x2D] = KEY_MINUS,
+    [0x2E] = KEY_EQUAL,
+    [0x2F] = KEY_LEFTBRACE,
+    [0x30] = KEY_RIGHTBRACE,
+    [0x31] = KEY_BACKSLASH,
+    [0x32] = KEY_SEMICOLON,
+    [0x33] = KEY_APOSTROPHE,
+    [0x34] = KEY_GRAVE,
+    [0x35] = KEY_COMMA,
+    [0x36] = KEY_DOT,
+    [0x37] = KEY_SLASH,
+    [0x38] = KEY_CAPSLOCK,
+    [0x39] = KEY_F1,
+    [0x3A] = KEY_F2,
+    [0x3B] = KEY_F3,
+    [0x3C] = KEY_F4,
+    [0x3D] = KEY_F5,
+    [0x3E] = KEY_F6,
+    [0x3F] = KEY_F7,
+    [0x40] = KEY_F8,
+    [0x41] = KEY_F9,
+    [0x42] = KEY_F10,
+    [0x43] = KEY_F11,
+    [0x44] = KEY_F12
+};
+
 int parse_hid_report(struct usb_keyboard *keyboard, unsigned char *data, int size)
 {
 	struct input_dev *input;
@@ -187,10 +256,8 @@ int parse_hid_report(struct usb_keyboard *keyboard, unsigned char *data, int siz
 	//Key down
 	for (i = 0; i < 6; i++) {
 		if (keycode[i]) { 
-			pr_debug("Keycode -> %x\n", keycode[i]);
-			if (keycode[i] == 0x14)
-				input_report_key(input, KEY_Q, KEY_DOWN);
-			input_report_key(input, keycode[i], KEY_DOWN);
+			input_report_key(input, hid_to_linux_keycode[keycode[i]], KEY_DOWN);
+			//input_report_key(input, keycode[i], KEY_DOWN);
 		}
 	}
 	input_sync(input);
